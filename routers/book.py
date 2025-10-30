44fae669b8945954fce55ff24a3f6ef216d928a3
rodrigoskv <rodrigotelesskv@gmail.com>
2025-10-30 17:32:10 -0300

    alguns testes com um banco temporário e criação de schemas

--- routers/book.py
+++ routers/book.py
@@ -1,14 +1,17 @@
 from http import HTTPStatus
 from fastapi import APIRouter
-from schema.book.book import BookCreate
+from schema.book.book import *
 
 router = APIRouter()
 
-@router.post("/books/", status_code= HTTPStatus.CREATED)
-def create_book(book: BookCreate):
-    breakpoint()
-    return book
+database=[]
 
+@router.post("/books/", status_code= HTTPStatus.CREATED, response_model=BookPublic)
+def create_book(book: BookSchema):
+    book_with_id=BookId(**book.model_dump(), id=len(database)+1) #** desempacota o dicionario(separa por argumentos e entrega por parametros corretos)
+    #da no mesmo escrever um
+    #book_with_id=BookId(title=book.title,author=book.author, published_year=book.published_year, isbn=book.isbn, id=len(database)+1)
+    database.append(book_with_id)
+    return book_with_id
 
-
-
+# @router.get("/books/get", status_code=HTTPStatus.ACCEPTED, responses=BookGet)
--- schema/book/book.py
+++ schema/book/book.py
@@ -2,19 +2,23 @@ from typing import Optional
 import model
 from schema import BaseModel
 
-class BookCreate(BaseModel):
-    id : int
+class BookSchema(BaseModel):
     title : str
     author : str
-    published_year : str
+    published_year : int
     isbn : str
+
+class BookPublic(BaseModel):
+    id : int
+    title: str
+    author: str
+    published_year: int
+    isbn: str
+
+class BookId(BookSchema):
+    id:int
 
 class BookUpdate(BaseModel):
     title : Optional[str] = None
     author : Optional [str] = None
-    published_year : Optional[str]= None
-
-class BookGet(BaseModel):
-    id : int
-    title : str
-    author : str
+    published_year : Optional[int]= None
